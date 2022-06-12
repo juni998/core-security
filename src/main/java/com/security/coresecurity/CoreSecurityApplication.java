fbb2513ba6216723e8747356e324b754172af737
aou101 <wodnrdlgla@gmail.com>
2022-06-12 21:06:34 +0900

    #4 기본 controller 생성

--- src/main/java/com/security/coresecurity/CoreSecurityApplication.java
+++ src/main/java/com/security/coresecurity/CoreSecurityApplication.java
@@ -2,6 +2,17 @@ package com.security.coresecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 public class CoreSecurityApplication {
@@ -9,5 +20,6 @@ public class CoreSecurityApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CoreSecurityApplication.class, args);
 	}
+
 
 }
